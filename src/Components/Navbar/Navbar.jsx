57aa4510920d7bea16b9f7570d1467afa25d807c
Abhay <abhiynar2804@gmail.com>
2026-01-25 11:56:28 +0530

    fixed logo name in navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './Navbar.css'
-import logo from '../../assets/Logo.png';
+import logo from '../../assets/logo.png';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 
 const Navbar = () => {
